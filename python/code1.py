57df92c025abe9442ea26e5984b8f57b237aa0d4
David Sye <davidsye21@gmail.com>
2019-06-04 14:27:55 -0400

    upload new exercise to github

---
+++ python/code1.py
@@ -0,0 +1,5 @@
+address_info = {"recipient_name": "David Sye", "street_address": "101 Moon St.", "city_name": "Moon City", "state": "The Moon", "zip": "00000"}
+print(address_info["recipient_name"])
+print(address_info["street_address"])
+print(address_info["city_name"] + ", " + address_info["state"] + " " + address_info["zip"])
+#This is a fake address. Unfortunately, I do not live on the moon :( 
