9bbaaa8947a2385ce99e824f255de7a7097f2810
David Sye <davidsye21@gmail.com>
2019-06-04 14:31:43 -0400

    update

--- python/my_info.py
+++ python/my_info.py
@@ -1,1 +1,1 @@
-my_info={"first_name": "David", "last_name":"Sye", "hometown": "Arlington Heights, IL", "favorite_food":"steak"}
+my_info={"first_name": "David", "last_name":"Sye", "hometown": "---------", "favorite_food":"steak"}
---
+++ python/workspace.py
@@ -0,0 +1,8 @@
+vegetables = ["brocolli", "celery", "radishes", "beets"]
+my_vegetables = ["brocolli", "celery", "radishes"]
+my_vegetables = vegetables[0], vegetables[1], vegetables[3]
+
+doctors = ['Matt Smith', "David Tennent"]
+best_episode= "Blink"
+print("My favorite episode is " + best_episode + ".")
+print("My favorite doctor is " + doctors[1] + ".")
