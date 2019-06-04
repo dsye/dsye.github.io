0b0c82c3914bf164028380bb9063005f44e365d5
David Sye <davidsye21@gmail.com>
2019-06-04 10:26:32 -0400

    Created a file practicing print and variable functions.

---
+++ python/group_practice.py
@@ -0,0 +1,7 @@
+print("This is a test, with my partner Leeah.")
+print("We will be assigning variables to terms.")
+Public_History = 23
+Library_Science = 30
+Total_Credit_Hours = Public_History + Library_Science
+print("Below is the total credit hours for the Public History/Library Science Dual Degree program at IUPUI.")
+print(Total_Credit_Hours)
