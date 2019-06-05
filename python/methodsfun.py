b001c09ed7f5800abbbfc57c53ad0f0637cd1b91
David Sye <davidsye21@gmail.com>
2019-06-05 11:22:20 -0400

    more methods work

--- python/methodsfun.py
+++ python/methodsfun.py
@@ -1,17 +1,17 @@
-def intro(name):
-    print(name + " is Master of the Universe!")
-
-def not_d(name):
-    intro(name)
-    print("According to the New York Times this is factually incorrect.")
-
-people = ["David", "Leeah", "Ethan"]
-for name in people:
-    if name == "David":
-        intro(name)
-        print("Long may he reign!")
-    else:
-        not_d(name)
+# def intro(name):
+#     print(name + " is Master of the Universe!")
+#
+# def not_d(name):
+#     intro(name)
+#     print("According to the New York Times this is factually incorrect.")
+#
+# people = ["David", "Leeah", "Ethan"]
+# for name in people:
+#     if name == "David":
+#         intro(name)
+#         print("Long may he reign!")
+#     else:
+#         not_d(name)
 
 # def method_one():
 #     print("look I'm a method")
@@ -21,3 +21,10 @@ for name in people:
 #     method_one()
 #
 # method_two("Brandon")
+
+def workpub(works):
+    for title, date in works.items():
+        print(title + " was published in " + date + ".")
+
+works = {"Jane Eyre": "1847", "Cane": "1923", "Wide Sargasso Sea":"1966", "Citizen: An American Lyric":"2014"}
+workpub(works)
